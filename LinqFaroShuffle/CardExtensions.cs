9b67a39d3d2e3db298c9241472a9b91ad40e4f55
samyfg41 <samanthafaruolo@gmail.com>
2026-03-16 11:47:37 -0300

    Adicionando aula LINQ.

---
+++ LinqFaroShuffle/CardExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace LinqFaroShuffle;
+
+public static class CardExtensions
+{
+    extension<T>(IEnumerable<T> sequence)
+    {
+        public IEnumerable<T> InterleaveSequenceWith(IEnumerable<T> second)
+        {
+            var firstIter = sequence.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                yield return firstIter.Current;
+                yield return secondIter.Current;
+            }
+        }
+    }
+}
